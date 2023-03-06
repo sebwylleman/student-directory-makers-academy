4f5d5af1e167c3259a22cbcf7f42f8d80f0c2a2c
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-06 08:55:28 +0000

    indexed students hash

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
+
+# We're using the each() method to iterate over an array of students.
+#  How can you modify the program to print a number before the name of each student,
+#   e.g. "1. Dr. Hannibal Lecter"? Hint: look into each_with_index()
+
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
-
 
 def print_header
   puts "The students of Villains Academy"
@@ -24,8 +24,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -33,8 +33,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-#nothing happens until we call the methods
 students = input_students
-print_header
+print_header()
 print(students)
 print_footer(students)
