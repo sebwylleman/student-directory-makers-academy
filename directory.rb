8310d20bab1a82a89fadc1a36d3a60acccb458e1
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-03-07 10:20:07 +0000

    adding iteractive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names and cohorts of the students, separated by a comma"
   puts "To finish, just hit return twice"
@@ -39,7 +64,4 @@ def print_footer(names)
   end
 end
 
-students = input_students
-print_header()
-print(students)
-print_footer(students)
+interactive_menu()
